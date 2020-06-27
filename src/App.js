06003c78abb9074ac22567cac93084b15a54524f
Michael Perez <mperez6473@gmail.com>
2020-06-27 15:31:01 -0500

    Added the start of a store that is fetching data with a reducer and acions folder and files. Moved some folders around as well. Have the hook for useLocalStorage going. Added allMenuItems fetching from the fetchData action.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRouter";
 
 function App() {
+    const token = localStorage.getItem("token");
+    const [loggedIn, setLoggedIn] = useState(token && true);
+    document.title = "Dev-Desk-Queue";
     return (
-        <div className="App">
-            <h1>This is the home page in progress</h1>
-        </div>
+        <>
+            <Switch></Switch>
+        </>
     );
 }
 
