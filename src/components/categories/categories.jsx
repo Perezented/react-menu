3764a8bc5c1404609d4fe970bb42d22f78bca5f5
Michael Perez <mperez6473@gmail.com>
2020-09-08 13:53:10 -0500

    Categories display fix.

--- src/components/categories/categories.jsx
+++ src/components/categories/categories.jsx
@@ -7,68 +7,69 @@ import { connect } from "react-redux";
 import { fetchMenuData } from "../../store/actions";
 
 const Categories = (props) => {
-    const { push } = useHistory();
-    useEffect(() => {
-        props.fetchDeeperMenuItems("categories");
-    }, []);
-    // props.categoriesArray &&
-    //     console.log("categories props", props);
+  const { push } = useHistory();
+  useEffect(() => {
+    props.fetchDeeperMenuItems("categories");
+  }, []);
+  // props.categoriesArray &&
+  //     console.log("categories props", props);
 
-    return (
-        <section>
-            <div
-                className="singleCat allMenuButton"
+  return (
+    <section>
+      <div
+        className="singleCat allMenuButton"
+        onClick={() => {
+          push("/");
+          props.fetchMenuData("menu");
+        }}
+      >
+        <Link to="/">All Menu Items</Link>{" "}
+      </div>{" "}
+      <div className="categories">
+        {!props.categoriesArray ? (
+          <h3>Loading, Please Wait...</h3>
+        ) : (
+          props.categoriesArray.categories.map((value, key) => {
+            const linkValue = `categories?filter=${value.category}`;
+            const linkValueWithMenu = `menu/categories?filter=${value.category}`;
+            return (
+              <div
+                key={key}
+                className="singleCat"
                 onClick={() => {
-                    push("/");
-                    props.fetchMenuData("menu");
-                }}
-            >
-                <Link to="/">All Menu Items</Link>{" "}
-            </div>{" "}
-            <div className="categories">
-                {!props.categoriesArray ? (
-                    <h3>Loading, Please Wait...</h3>
-                ) : (
-                    props.categoriesArray.categories.map((value, key) => {
-                        const linkValue = `categories?filter=${value.category}`;
-                        const linkValueWithMenu = `menu/categories?filter=${value.category}`;
-                        return (
-                            <div
-                                key={key}
-                                className="singleCat"
-                                onClick={() => {
-                                    // authenticatedAxios().get(linkValue).then((response)=>{
-                                    //     console.log(response)
+                  // authenticatedAxios().get(linkValue).then((response)=>{
+                  //     console.log(response)
 
-                                    // }).catch((err)=>{console.log(err);})
-                                    props.fetchMenuData(linkValueWithMenu);
-                                    // push(linkValue);
-                                    // push("menu");
-                                    // push(linkValue);
-                                }}
-                            >
-                                <Link to={linkValue}>
-                                    {value.categoryDescription}
-                                </Link>
-                            </div>
-                        );
-                    })
-                )}
-            </div>
-        </section>
-    );
+                  // }).catch((err)=>{console.log(err);})
+                  props.fetchMenuData(linkValueWithMenu);
+                  // push(linkValue);
+                  // push("menu");
+                  // push(linkValue);
+                }}
+              >
+                <Link to={linkValue}>
+                  {console.log(value.category)}
+                  {value.category}
+                </Link>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </section>
+  );
 };
 const mapStateToProps = (state) => {
-    // console.log('categories state', state)
-    return {
-        isFetching: state.categoriesFetchReducer.isFetching,
-        error: state.categoriesFetchReducer.error,
-        categoriesArray: state.categoriesFetchReducer.categoriesArray,
-        menuArray: state.menuFetchReducer.menuArray,
-    };
+  // console.log('categories state', state)
+  return {
+    isFetching: state.categoriesFetchReducer.isFetching,
+    error: state.categoriesFetchReducer.error,
+    categoriesArray: state.categoriesFetchReducer.categoriesArray,
+    menuArray: state.menuFetchReducer.menuArray,
+  };
 };
 
 export default connect(mapStateToProps, {
-    fetchDeeperMenuItems,
-    fetchMenuData,
+  fetchDeeperMenuItems,
+  fetchMenuData,
 })(Categories);
