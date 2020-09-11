8d81babb200e57183868b45ba42ee624a9926e57
Michael Perez <mperez6473@gmail.com>
2020-09-11 02:25:27 -0500

    Adding side cart ability to send order to back end

--- src/components/sideCart/sideCart.jsx
+++ src/components/sideCart/sideCart.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { pushData } from "../../store/actions/index";
 
 const SideCart = (props) => {
+  console.log(props);
+  useEffect(() => {
+    props.pushData();
+  }, [props.pushData]);
   let total = 0;
   // let what_is_in_cart = {};
   let key_num = 0;
-
+  let curr_dict = props.cart.dict;
+  const today = new Date();
+  const date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const orderNumber = today.getMilliseconds();
+  const dateTime = date + " " + time;
+  function makeObj(dictionary) {
+    Object.keys(dictionary).forEach(function (key) {
+      if (dictionary[key].amount !== 0) {
+        let item = {};
+        item["total"] = total;
+        item["menuItemID"] = dictionary[key].menuItemID;
+        item["quantity"] = dictionary[key].amount;
+        item["created_at"] = dateTime;
+        item["orderID"] = orderNumber;
+        console.log(item);
+        props.pushData(item);
+      }
+      // dictionary.preventDefault();
+    });
+  }
   return (
     <section className="sideCart">
       <h3>Your Shopping Cart So Far</h3>
@@ -14,16 +40,6 @@ const SideCart = (props) => {
         props.cart.cart.map((value, i, a) => {
           total += value.price * value.amount;
           key_num += 1;
-          // if (what_is_in_cart[value.menuItemID] === undefined) {
-          // }
-          // a.filter((filteredValue) => {
-          // });
-          // a.map((listedMenuItems) => {
-          //     if (listedMenuItems.menuItemID) {
-          //         let totalAmt =
-          //             listedMenuItems.amount + value.amount;
-          //     }
-          // });
           if (true) {
             if (value.amount !== 0) {
               return (
@@ -45,10 +61,16 @@ const SideCart = (props) => {
         <h5 className="orderedItem">Total: {total.toFixed(2)}</h5>
       )}
       {props.cart.cart.length > 0 && (
-        <button onClick={() => {}}>Submit Order</button>
+        <button
+          onClick={() => {
+            makeObj(curr_dict);
+          }}
+        >
+          Submit Order
+        </button>
       )}
     </section>
   );
 };
 
-export default connect(null, {})(SideCart);
+export default connect(null, { pushData })(SideCart);
