5c52518a64cbe7e59dd094b51884d62a58c7e4fa
Michael Perez <mperez6473@gmail.com>
2020-10-29 11:59:19 -0500

    Moved around the way the routes were placed in App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 // import PrivateRoute from "./utils/privateRouter";
-
 import Header from "./components/header/header";
 import AllMenuItems from "./components/menuItems/menuItems";
 import SpecialOfTheDay from "./components/specialOfTheDay/specialOfTheDay";
@@ -48,13 +46,12 @@ function App(props) {
       <Route exact path="/">
         <HomePage />
       </Route>
-      <div className="imgBackground" />
-      <Route path="/order">
-        <div className="App">
-          <Header />
+      <div className="App">
+        <Header />
+        <Route path="/order">
           <div className="spacer">
+            <SpecialOfTheDay />
             <Route exact path="/order">
-              <SpecialOfTheDay />
               <Welcome />
             </Route>
             <Route exact path="/order">
@@ -64,18 +61,18 @@ function App(props) {
               <Route exact path="/order">
                 <AllMenuItems cart={{ cart, removeItem, addItem, dict }} />
               </Route>
-              <Route exact path="/orderdetails">
-                <OrderDetails />
-              </Route>
-              <Route path="/order" />
             </Switch>
           </div>
-        </div>
-      </Route>
+        </Route>
+        <Route path="/orderDetails">
+          <div className="spacer">
+            <SpecialOfTheDay />
+          </div>
+          <OrderDetails />
+        </Route>
+      </div>{" "}
     </>
   );
 }
-
 export default App;
-
 //<span>Photo by <a href="https://unsplash.com/@timothylbrock?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Timothy L Brock</a> on <a href="https://unsplash.com/s/photos/mexican-food?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
